da5f8fbfc596dc8da480aed17d4ea2e0d8295a97
Stephen Schier <stephenjschier@gmail.com>
2025-06-01 15:29:33 -0500

    added renderHost

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const renderHost = 'https://candidatesearch-bopm.onrender.com';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: './env',
@@ -9,5 +11,6 @@ export default defineConfig({
   preview: {
     port: parseInt(process.env.PORT || '4173'),
     host: true,
+    allowedHosts: [renderHost],
   },
 });
